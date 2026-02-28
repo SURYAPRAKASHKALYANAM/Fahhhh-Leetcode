83e962c53b0aca4e0bb6490cb3af4a9da45d424f
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2026-02-28 10:56:19 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,79 @@
+let audioEnabled = false;
+let isWaitingForSubmission = false;
+
+// 1. Unlock Audio Context
+window.addEventListener('click', () => {
+    if (!audioEnabled) {
+        audioEnabled = true;
+        console.log("🔊 Audio Context Unlocked");
+    }
+});
+
+// 2. Listen for the SUBMIT button click
+document.addEventListener('click', (e) => {
+    const isSubmitBtn = e.target.innerText === "Submit" || 
+    e.target.closest('button')?.innerText.includes("Submit");
+    
+    if (isSubmitBtn) {
+        console.log("🚀 Submission sent! Waiting for results...");
+        isWaitingForSubmission = true;
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+});
+
+// 3. Play function using Storage
+const playSelectedMeme = (key) => {
+    chrome.storage.local.get([key, 'volume', 'duration'], (data) => {
+        const filename = data[key] || (key === 'successMeme' ? 'spiderman-meme-song.mp3' : 'fahhhhh.mp3');
+        const audio = new Audio(chrome.runtime.getURL(`sounds/${filename}`));
+        
+        audio.volume = data.volume || 0.5;
+        audio.play();
+
+        // Stop audio after X seconds
+        const limit = (data.duration || 5) * 1000;
+        setTimeout(() => {
+            audio.pause();
+            audio.currentTime = 0;
+        }, limit);
+    });
+    observer.disconnect(); // Stop observing until next submission
+};
+
+
+// 4. Robust Observer
+const observer = new MutationObserver(() => {
+    if (!isWaitingForSubmission) return;
+
+    // Check for "Accepted" using the specific class you identified
+    const successSpan = document.querySelector('span.marked_as_success');
+    
+    // Check for "Wrong Answer" in headers
+    const headers = document.querySelectorAll('h3');
+    let wrongAnswerFound = false;
+    for (let h3 of headers) {
+        if (h3.innerText.includes("Wrong Answer")) {
+            wrongAnswerFound = true;
+            break;
+        }
+    }
+
+    const errorSpan = document.querySelector('span[data-e2e-locator="console-result"]');
+    // 3. Check for Runtime Error (The new Span you found)
+    const isRuntimeError = errorSpan && errorSpan.innerText.includes("Runtime Error");
+
+    // 4 . compile Error check (The new Span you found)
+    const isCompileError = errorSpan && errorSpan.innerText.includes("Compile Error");
+
+    // 5. TLE 
+    const isTLE = errorSpan && errorSpan.innerText.includes("Time Limit Exceeded");    
+    if (successSpan) {
+        console.log("Trigger:  Succcess! 🕷️");
+        playSelectedMeme('successMeme');
+        isWaitingForSubmission = false;
+    } else if (wrongAnswerFound || isRuntimeError || isCompileError || isTLE) {
+        console.log("Trigger: Failed! 😫");
+        playSelectedMeme('failMeme');
+        isWaitingForSubmission = false;
+    }
+});
